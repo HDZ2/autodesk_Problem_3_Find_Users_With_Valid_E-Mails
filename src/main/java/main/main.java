d4e9dca9ffac4b39b3eb2e1aacf5cd8e073bd45f
HDZ2 <100626951+HDZ2@users.noreply.github.com>
2024-04-17 11:57:38 -0600

    Update main.java
    Moved description so that it can be easier to spot

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -4,18 +4,17 @@ import methods.validateEmail;
 
 import java.util.Scanner;
 
-/*
-*
-* This is the main class where I first ask how many emails are going to be entered and save it in to variable
-* Once that's set, I itirate and ask for each email and save it into email variable
-* Once the email is saved I call the function isEmailValid which I call up from validateEmail class
-* Once the result is obtained I print a message based on its value
-*
-* */
-
-
 public class main {
 
+    /*
+    *
+    * This is the main class where I first ask how many emails are going to be entered and save it in to variable
+    * Once that's set, I itirate and ask for each email and save it into email variable
+    * Once the email is saved I call the function isEmailValid which I call up from validateEmail class
+    * Once the result is obtained I print a message based on its value
+    *
+    * */
+    
     public static void main(String[] args) {
         //Constructor to invoke the isvalidEmail function
         validateEmail validEmail = new validateEmail();
